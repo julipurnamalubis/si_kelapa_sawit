f74edb398d423df7f6f86019f7085b8d88de1842
julipurnamalubis <77524878+julipurnamalubis@users.noreply.github.com>
2021-01-17 12:00:47 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> PT.BCD |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Kelapa Sawit</a>
+            <a href="#">Support</a>
+        </div>
+    </div>
+</div>
